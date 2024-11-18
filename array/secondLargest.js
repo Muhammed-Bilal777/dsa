394bad8e4b79abf3d6b13e19c61408475be44158
muhammed Bilal <mdbilaal777@gmail.com>
2024-11-18 10:22:33 +0530

    second largest element

--- array/secondLargest.js
+++ array/secondLargest.js
@@ -1,9 +1,24 @@
 //second Largest in array
 
+// function secondLargest(arr) {
+//   const sortedArr = arr.sort((a, b) => a - b);
+//   return sortedArr[arr.length - 2];
+// }
+
 function secondLargest(arr) {
-  const sortedArr = arr.sort((a, b) => a - b);
-  return sortedArr[arr.length - 2];
+  let max = -Infinity;
+  let secondMax = -Infinity;
+  for (let item of arr) {
+    if (item > max) {
+      secondMax = max;
+      max = item;
+    } else if (item > secondMax && item !== max) {
+      secondMax = item;
+    }
+  }
+
+  return secondMax;
 }
 
-const arr = [1, 3, 44, 5, 6, 77, 88, 4, 22, 111];
+const arr = [1, 3, 44, 5, 6, 77, 88, 4, 22, 111, 88];
 console.log(secondLargest(arr));
